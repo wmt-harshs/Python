6d2083978b9ad5c3a37f55f738cde2e18c5617ad
wmt-harshs <harshs@webmob.tech>
2022-02-07 16:13:06 +0530

    Diffrent types of string

---
+++ Variable_and_data_type/String.py
@@ -0,0 +1,27 @@
+
+print("Harsh Solanki")
+print("Harsh\nSolanki")
+print("Harsh\"Solanki")
+
+string = "Harsh Solanki"
+print(string)
+
+#Concatination
+string = "Harsh Solanki "
+print(string + "......")
+
+#Convert string into lower and upper case
+string = "Harsh Solanki"
+print(string.lower() + "\t\t" + string.upper())
+print(string.isupper())
+
+#length of function
+print(len(string))
+
+#print specific value
+print(string[0])
+print(string.index("S"))
+
+#Replace string
+print(string.replace("Solanki" , "solanki"))
+
