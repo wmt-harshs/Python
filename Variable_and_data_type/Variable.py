83501e5b5d3e6478781f4dad716249f657795969
wmt-harshs <harshs@webmob.tech>
2022-02-07 15:35:43 +0530

    script for variables

---
+++ Variable_and_data_type/Variable.py
@@ -0,0 +1,11 @@
+varuable_name = "Harsh"
+Variable_age = "21"
+
+print("Hello, my name is "+ varuable_name + ".")
+print("My age is "+ Variable_age +".")
+
+varuable_name = "krunal"
+Variable_age = "21"
+
+print("Hello, my name is "+ varuable_name + ".")
+print("My age is "+ Variable_age +".")
