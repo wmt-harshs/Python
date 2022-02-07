c5c44e15cb0d99f8ce07b596de0c6b430695f6cb
wmt-harshs <harshs@webmob.tech>
2022-02-07 17:41:37 +0530

    script of diffrent number varable code

---
+++ Variable_and_data_type/Number.py
@@ -0,0 +1,33 @@
+print(10)
+print(3 + 4) # + - * / %
+print(3 * ( 4 + 5 ))
+
+my_num = 21
+print("my age: " + str(my_num))
+
+#show onlu absolute value
+my_num = -21
+print(abs(my_num))
+
+#power function
+print(pow( 2 , 3 ))
+
+#max-min
+print( "max_value =" , max( 2 , 3 ))
+print( "min_value =" , min( 2 , 3 ))
+
+#round
+print( "round_value =" , round( 2.6 ))
+
+#importing more mathematics function
+from math import *
+
+#example of floor and ceil 
+print(floor(4.6) , ceil(4.6))
+
+#sqrt
+sqrt = sqrt(16)
+print("sqrt:-" , sqrt)
+
+
+
