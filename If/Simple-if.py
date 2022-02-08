2d83018c0456ea31c6efd434ed35d0e8645903da
wmt-harshs <harshs@webmob.tech>
2022-02-08 15:14:48 +0530

    Simple if-else script

---
+++ If/Simple-if.py
@@ -0,0 +1,16 @@
+
+Is_male = False           #input("Is you male:- ")
+Is_tall = False         #input("Is you tall:- ")
+
+if Is_male and Is_tall:
+    print("You are tall male.")
+
+elif Is_male and not(Is_tall):  
+    print("You are short male.")
+
+elif not(Is_male) and Is_tall:  
+    print("You are not male but you are tall.")
+
+else:
+    print("You are nither male nor tall.")
+
