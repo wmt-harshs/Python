4ef5011f8e1ca5774113769881d1cd230bcb114c
wmt-harshs <harshs@webmob.tech>
2022-02-08 10:33:12 +0530

    script for mad libs game

---
+++ Game/Mad-Libs-Game.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+colour = input("Enter colour name: ")
+series = input("Enter series name: ")
+
+print("Hello,my name is " + name)
+print("my favrite colour is " + colour)
+print("And I am watching " + series + " series in youtube.")
