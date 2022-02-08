841ec5d7b194353f50a8e5772131515a12f8f0a3
wmt-harshs <harshs@webmob.tech>
2022-02-08 12:45:21 +0530

    Simple function script

---
+++ Function/Simple-function.py
@@ -0,0 +1,6 @@
+def Full_name(name,age):
+    #  name = input("Enter your name:- ")
+    #  age = input("Enter your age:- ")
+     print("name:-" + name + "   age:-" + age)
+
+Full_name("Harsh","21")    
