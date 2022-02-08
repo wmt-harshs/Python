04f0e70945cd87e36e63341e6fce4f566336a447
wmt-harshs <harshs@webmob.tech>
2022-02-08 11:25:34 +0530

    Diffrent script for list-function

---
+++ Lists/List-function.py
@@ -0,0 +1,36 @@
+Lucky_number = [51,5,12,515,21,2]
+friends = ["Ankur", "Krunal", "Meet", "Deep", "Jay"]
+
+# merge two lists
+# friends.extend(Lucky_number)
+
+#add item in list 
+# friends.append("Ajit")
+
+#add item at specifc number 
+# friends.insert(1,"Gaurav")
+
+#remove item
+# friends.remove("Jay")
+
+#delete all item in list
+# friends.clear()
+
+#pop last item in list
+# friends.pop()
+
+#to check item in list
+# print(friends.index("Deep"))
+
+#To count how many times are same 
+# print(friends.count("Meet"))
+
+#sort
+# friends.sort()
+# Lucky_number.sort()
+# print(friends)
+# print(Lucky_number)
+
+#copy list in another list
+#  friends2 = friends.copy()
+#  print(friends)
