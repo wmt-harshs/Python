9bb33b4db617c1e3a2a8faac469becf700cf29da
wmt-harshs <harshs@webmob.tech>
2022-02-09 16:35:21 +0530

    Create Translater

---
+++ Loops/For/Building_Trancelater.py
@@ -0,0 +1,14 @@
+
+def trancelater(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(trancelater(input("Enter phrase:- ")))
