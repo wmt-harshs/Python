c5ed71442eb5a2a5fd122215c6d82ddd5c297fb5
wmt-harshs <harshs@webmob.tech>
2022-02-09 12:33:34 +0530

    Simple Dictionaries script

---
+++ Dictionaries/Dictionaries.py
@@ -0,0 +1,18 @@
+
+Months={
+
+    1:"January",
+    2:"Fabruary",
+    3:"March",
+    4:"April",
+    5:"May",
+    6:"Jun",
+    7:"July",
+    8:"August",
+    9:"September",
+    10:"Octomber",
+    11:"Novmber",
+    12:"December",
+}
+Num = int(input("Enter number:- "))
+print(Months.get(Num))
