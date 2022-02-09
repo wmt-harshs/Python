6e2068d9c2706808f3108689b6b1e69ea1f79cca
wmt-harshs <harshs@webmob.tech>
2022-02-09 14:45:58 +0530

    simple for loop scripts

---
+++ Loops/For/SimpleForLoop.py
@@ -0,0 +1,11 @@
+
+# for letter in "Harsh Solanki":
+#     print(letter)
+
+Friends = ["Krunal", "Ankur", "Deep", "Meet"]
+
+for index in range(len(Friends)):
+    print(Friends[index])
+
+# for index in range(3,10):
+#     print(index)
