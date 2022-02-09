612e36bfbbc2a31be8f0017c55eafca0f9c5f593
wmt-harshs <harshs@webmob.tech>
2022-02-09 16:01:08 +0530

    Nested for loop

---
+++ Loops/For/Nested_For.py
@@ -0,0 +1,15 @@
+two_dimention_list = [
+     
+     [2,5,8],
+     [1,4,7],
+     [3,6,9],
+     [0]
+
+]
+
+# print(two_dimention_list[2][1])
+
+for row in two_dimention_list:
+    for coloum in row:
+        print(coloum)
+
