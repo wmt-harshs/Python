168c9cabc9b7ad2a92acd437dab0d8cadb65b93c
wmt-harshs <harshs@webmob.tech>
2022-02-10 17:24:06 +0530

    Simple class

---
+++ Class/Simple_Class/Student_class.py
@@ -0,0 +1,8 @@
+class Student:
+
+    def __init__(self, name, mobile_number , gpa , Working_in_a_Project):
+        self.name = name
+        self.mobile_number = mobile_number
+        self.gpa = gpa
+        self.Working_in_a_Project = Working_in_a_Project
+        
---
+++ Class/Simple_Class/Student_obj.py
@@ -0,0 +1,6 @@
+from Student_class import Student
+
+student1 = Student("Harsh", 4565484314, 8.5, True)
+
+print(student1.name)
+# print(student1.name)
