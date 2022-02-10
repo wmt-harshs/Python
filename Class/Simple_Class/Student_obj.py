5255656e350921a82043ba497b18a66a40934083
wmt-harshs <harshs@webmob.tech>
2022-02-10 17:24:36 +0530

    Simple class

--- Class/Simple_Class/Student_obj.py
+++ Class/Simple_Class/Student_obj.py
@@ -1,6 +1,5 @@
-from Student_class import Student
+from Class.Simple_Class.Student_class import Student
 
 student1 = Student("Harsh", 4565484314, 8.5, True)
 
 print(student1.name)
-# print(student1.name)
