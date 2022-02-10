86a00aa8f908edf519ffcc50e7034805d2bf0e95
wmt-harshs <harshs@webmob.tech>
2022-02-10 16:42:04 +0530

    Initial Commit

---
+++ Class/Class_with_Function/Class.py
@@ -0,0 +1,13 @@
+
+class Number:
+
+    def __init__(self,number):
+        
+        self.number = number
+
+    def Check_number(self):
+        if len(self.number) == 10:
+            print("Valid Number") 
+        else:
+            print("Invalid Number")
+
