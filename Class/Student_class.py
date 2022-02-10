c883cbcba2f0945d6ecb4943bbd3a062980b2e6e
wmt-harshs <97028947+wmt-harshs@users.noreply.github.com>
2022-02-10 17:22:43 +0530

    Delete Student_class.py

--- Class/Student_class.py
+++
@@ -1,8 +0,0 @@
-class Student:
-
-    def __init__(self, name, mobile_number , gpa , Working_in_a_Project):
-        self.name = name
-        self.mobile_number = mobile_number
-        self.gpa = gpa
-        self.Working_in_a_Project = Working_in_a_Project
-        
